b633fc3f4c3c7cae1dece7ee87b346e388c4c07f
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-02-08 13:05:31 +0100

    Ts - exploring the base types

---
+++ 027-react-typescript/plain/basics.ts
@@ -0,0 +1,19 @@
+/* 
+  Primitives; number, string, boolean
+  More complex types: arrays, objects
+  Function types, parameters
+*/
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName= "Aleksandar";
+
+let isInstructor: boolean;
+
+isInstructor = true;
