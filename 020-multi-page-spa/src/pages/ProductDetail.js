930ad3b8c4b64587ea76024ac0b81fdb09532716
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-02-18 14:19:50 +0100

    Extracting Route params

--- 020-multi-page-spa/src/pages/ProductDetail.js
+++ 020-multi-page-spa/src/pages/ProductDetail.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
+  const params = useParams();
+
+  console.log(params);
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
