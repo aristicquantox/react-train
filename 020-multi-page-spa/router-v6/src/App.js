aff87082aea28aa6d83348b975d984c8b275cb80
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-02-24 14:53:46 +0100

    React router v6 second example start

--- 020-multi-page-spa/router-v6/src/App.js
+++ 020-multi-page-spa/router-v6/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Redirect } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -11,10 +11,11 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome" element={<Welcome />} />
-
           <Route path="/products" element={<Products />} />
-
           <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
       </main>
