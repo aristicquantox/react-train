67ecea58e364cf24471520510df5649a5895a05e
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-02-25 15:58:42 +0100

    Preparing project for next steps, add firebase link

--- 014-http-request/src/App.js
+++ 014-http-request/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useCallback, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
+  const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN;
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -12,7 +14,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(`${FIREBASE_DOMAIN}/movies.json`);
 
       if (!response.ok) {
         throw new Error("Something went wrong!");
@@ -34,11 +36,15 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }, []);
+  }, [FIREBASE_DOMAIN]);
 
   useEffect(() => {
     fetchMovieHandler();
   }, [fetchMovieHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies</p>;
 
@@ -55,6 +61,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
