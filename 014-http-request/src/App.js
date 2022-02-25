c2ae16337c59dc6d5d14d727947ef1dc3def7b92
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-02-25 12:13:35 +0100

    Using async/await

--- 014-http-request/src/App.js
+++ 014-http-request/src/App.js
@@ -5,21 +5,19 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMovieHandler = useCallback(() => {
-    fetch("https://swapi.dev/api/films/")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      })
-      .catch((err) => console.log(err));
+  const fetchMovieHandler = useCallback(async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }, []);
 
   // useEffect(() => {
