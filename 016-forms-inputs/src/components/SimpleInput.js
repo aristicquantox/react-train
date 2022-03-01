ffff4a071d3530cef5fea9e4fae14dcedaf4f317
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-03-01 15:58:13 +0100

    Forms and user input

---
+++ 016-forms-inputs/src/components/SimpleInput.js
@@ -0,0 +1,23 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
