ccebf8747f4ae97911a88f79d32b0f556a0608a0
Aleksandar Ristic <aleksandar.ristic@quantox.com>
2022-03-01 10:05:56 +0100

    Setup project for custom hook

---
+++ 015-custom-hooks/project-hooks/src/components/NewTask/NewTask.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const enterTaskHandler = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${FIREBASE_DOMAIN}/tasks.json`, {
+        method: "POST",
+        body: JSON.stringify({ text: taskText }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
